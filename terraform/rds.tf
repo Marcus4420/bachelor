712359169b876b5affeb90e293e88ad4cbe5e704
Marcus Ellested <marcusellested02@gmail.com>
2026-01-26 10:50:51 +0100

    Updated database version

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -41,7 +41,7 @@ resource "aws_security_group" "db_sg" {
 resource "aws_db_instance" "postgres" {
   identifier        = "${var.project_name}-db"
   engine            = "postgres"
-  engine_version    = "16.2"
+  engine_version    = "16"
   instance_class    = "db.t3.micro"
   allocated_storage = 20
 
